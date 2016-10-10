69cc9ea97364cc895759f9d3ece9ab438b5f9e60
Alexander DeRieux <acd1797@vt.edu>
2016-10-10 12:31:17 -0400

    New ".gitignore" file. Now ignores all user-specific files.

--- ERIS/ERIS/app/build/generated/source/r/debug/com/mysampleapp/R.java
+++ ERIS/ERIS/app/build/generated/source/r/debug/com/mysampleapp/R.java
@@ -1960,8 +1960,6 @@ containing a value of this type.
         public static final int AlertDialog_AppCompat_Light=0x7f09007e;
         public static final int Animation_AppCompat_Dialog=0x7f09007f;
         public static final int Animation_AppCompat_DropDownUp=0x7f090080;
-        /**  Customize your theme here. 
-         */
         public static final int AppTheme=0x7f090081;
         public static final int Base_AlertDialog_AppCompat=0x7f090082;
         public static final int Base_AlertDialog_AppCompat_Light=0x7f090083;
