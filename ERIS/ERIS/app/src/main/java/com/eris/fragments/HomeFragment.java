eef7147d5d4ae53c88c12f5b1b9bfc49e949e16d
Alexander DeRieux <acd1797@vt.edu>
2016-10-10 23:03:41 -0400

    Incorporated demo navigation buttons. Once clicked, each button now instantiates it's corresponding demo fragment.

--- ERIS/ERIS/app/src/main/java/com/eris/fragments/HomeFragment.java
+++ ERIS/ERIS/app/src/main/java/com/eris/fragments/HomeFragment.java
@@ -3,14 +3,25 @@ package com.eris.fragments;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.eris.R;
 
 public class HomeFragment extends Fragment {
+
+    /*
+     * Private Members
+     */
+    private Button buttonDemoLocation;
+    private Button buttonDemoScene;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -25,6 +36,45 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // Initialize buttons.
+        buttonDemoLocation = (Button) root.findViewById(R.id.buttonDemoLocation);
+        buttonDemoScene = (Button) root.findViewById(R.id.buttonDemoScene);
+
+        // Return the root view.
+        return root;
+
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Instantiate the location demo.
+        buttonDemoLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                final Fragment fragment = Fragment.instantiate(getActivity(),DemoLocationFragment.class.getName());
+                fragmentManager.beginTransaction()
+                        .replace(R.id.main_fragment_container, fragment,DemoLocationFragment.class.getSimpleName())
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit();
+            }
+        });
+
+        // Instantiate the scene demo.
+        buttonDemoScene.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                final Fragment fragment = Fragment.instantiate(getActivity(),DemoSceneFragment.class.getName());
+                fragmentManager.beginTransaction()
+                        .replace(R.id.main_fragment_container, fragment,DemoSceneFragment.class.getSimpleName())
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit();
+            }
+        });
     }
 }
