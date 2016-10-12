e3a5d1bac43aeb6109af495df9a8d7edf5ea55ee
Alexander DeRieux <acd1797@vt.edu>
2016-10-11 22:39:00 -0400

    Added support for center map on location. Map now centers on a given location and requests permissions for accessing user location.

--- ERIS/ERIS/app/src/main/java/com/eris/fragments/DemoLocationFragment.java
+++ ERIS/ERIS/app/src/main/java/com/eris/fragments/DemoLocationFragment.java
@@ -1,11 +1,19 @@
 package com.eris.fragments;
 
 
+import android.Manifest;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,21 +23,27 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.eris.R;
+import com.eris.activities.MainActivity;
 import com.eris.adapters.ResponderListAdapter;
 import com.eris.classes.Responder;
+import com.eris.services.LocationService;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
-///**
-// * A simple {@link Fragment} subclass.
-// * Activities that contain this fragment must implement the
-// * {@link DemoLocationFragment.OnFragmentInteractionListener} interface
-// * to handle interaction events.
-// * Use the {@link DemoLocationFragment#newInstance} factory method to
-// * create an instance of this fragment.
-// */
+import java.util.Random;
+
+
 public class DemoLocationFragment extends Fragment implements OnMapReadyCallback {
+
+    /*
+     * Private members
+     */
+    private static final int ZOOM_LEVEL = 18;
+    private final int REQUEST_CODE_ENABLE_MY_LOCATION = 222;
+    private BroadcastReceiver receiver;
 
     /*
      * Private Members
@@ -37,6 +51,7 @@ public class DemoLocationFragment extends Fragment implements OnMapReadyCallback
     private Button buttonAddResponder;
     private ListView responderListView;
     private ResponderListAdapter responderListAdapter;
+    private GoogleMap googleMap;
 
     public DemoLocationFragment() {
         // Required empty public constructor
@@ -45,6 +60,29 @@ public class DemoLocationFragment extends Fragment implements OnMapReadyCallback
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Create broadcast receiver object.
+        this.receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+
+                // Determine which broadcast was sent.
+                switch ( intent.getAction() ) {
+
+                    // Updated Location
+                    case LocationService.BROADCAST_ACTION_LOCATION_UPDATE:
+                        LatLng location = new LatLng(
+                                intent.getDoubleExtra(LocationService.KEY_LOCATION_LATITUDE,0.0),
+                                intent.getDoubleExtra(LocationService.KEY_LOCATION_LONGITUDE,0.0)
+                        );
+                        Log.d("location","UPDATE: " + location);
+                        centerMapOnLocation(location);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        };
     }
 
     @Override
@@ -62,6 +100,9 @@ public class DemoLocationFragment extends Fragment implements OnMapReadyCallback
         final FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
         fragmentTransaction.add(R.id.map_fragment_container,fragment);
         fragmentTransaction.commit();
+
+        // Set this fragment as the callback center for Google Maps.
+        fragment.getMapAsync(this);
 
         // Return the root view.
         return root;
@@ -82,6 +123,9 @@ public class DemoLocationFragment extends Fragment implements OnMapReadyCallback
                 // Show responder location on map.
                 final Responder responder = responderListAdapter.getItem(i);
                 Toast.makeText(getActivity(),"Clicked: " + responder.firstName + " " + responder.lastName,Toast.LENGTH_SHORT).show();
+
+                LatLng location = new LatLng(37.229491,-80.421531);
+                centerMapOnLocation(location);
             }
         });
 
@@ -125,5 +169,62 @@ public class DemoLocationFragment extends Fragment implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
+        // Obtain reference to Google Map object.
+        this.googleMap = googleMap;
+
+        // Enable my-location
+        this.enableMyLocation();
+    }
+
+    /**
+     *
+     */
+    private void enableMyLocation() {
+
+        // User has pre-allowed location permissions.
+        if ( ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED )
+        {
+            // Turn my-location ON.
+            this.googleMap.setMyLocationEnabled(true);
+        }
+
+        // Need to prompt user to allow location permissions.
+        else  {
+            ActivityCompat.requestPermissions(this.getActivity(),
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_CODE_ENABLE_MY_LOCATION);
+        }
+    }
+
+    /**
+     *
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ENABLE_MY_LOCATION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    enableMyLocation();
+                } else {
+                    // Permission Denied
+                    Toast.makeText(getActivity(), "ENABLE_MY_LOCATION Denied", Toast.LENGTH_SHORT)
+                            .show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    /**
+     *
+     * @param location
+     */
+    public void centerMapOnLocation(LatLng location) {
+        Log.d("demo","HEY: "+this.googleMap);
+        this.googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location, ZOOM_LEVEL));
     }
 }
