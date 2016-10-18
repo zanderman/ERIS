878ac3415d296883299a90d5575a1cdc282aab49
dpickle2 <dpickle29@gmail.com>
2016-10-18 17:00:15 -0400

    Add structure/references to responder DB fragment

--- ERIS/ERIS/app/src/main/java/com/eris/fragments/DemoResponderDatabaseFragment.java
+++ ERIS/ERIS/app/src/main/java/com/eris/fragments/DemoResponderDatabaseFragment.java
@@ -6,14 +6,32 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ScrollView;
 
 import com.eris.R;
+import com.eris.classes.PrototypeDatabaseHelper;
+import com.eris.classes.Responder;
 
 /**
  * Created by Will Schrag on 10/16/2016.
  */
 
 public class DemoResponderDatabaseFragment extends Fragment{
+
+    /*
+     * Private Members
+     */
+    private EditText responderNameField;
+    private EditText responderOrganizationField;
+    private EditText responderOrgSuperiorField;
+    private EditText responderLatitudeField;
+    private EditText responderLongitudeField;
+    private EditText responderHeartbeatField;
+    private EditText responderOrgSubordinatesField;
+    private EditText responderIncidentIdField;
+    private EditText responderIncidentSuperiorField;
+    private EditText responderIncidentSubordinatesField;
 
     public DemoResponderDatabaseFragment() {
         //Required?
@@ -28,7 +46,36 @@ public class DemoResponderDatabaseFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_demo_responder_database, container, false);
+        View root = inflater.inflate(R.layout.fragment_demo_responder_database, container, false);
+
+        // Initialize UI elements
+        responderNameField = (EditText) root.findViewById(R.id.responderNameField);
+        responderOrganizationField = (EditText) root.findViewById(R.id.responderOrganizationField);
+        responderOrgSuperiorField = (EditText) root.findViewById(R.id.responderOrgSuperiorField);
+        responderLatitudeField = (EditText) root.findViewById(R.id.responderLatitudeField);
+        responderLongitudeField = (EditText) root.findViewById(R.id.responderLongitudeField);
+        responderHeartbeatField = (EditText) root.findViewById(R.id.responderHeartbeatField);
+        responderOrgSubordinatesField = (EditText) root.findViewById(R.id.responderOrgSubordinatesField);
+        responderIncidentIdField = (EditText) root.findViewById(R.id.responderIncidentIdField);
+        responderIncidentSuperiorField = (EditText) root.findViewById(R.id.responderIncidentSuperiorField);
+        responderIncidentSubordinatesField = (EditText) root.findViewById(R.id.responderIncidentSubordinatesField);
+
+        // TODO this is temporary
+        PrototypeDatabaseHelper dbHelper = new PrototypeDatabaseHelper();
+        Responder responder = dbHelper.getResponderData("4093820716");
+        responderNameField.setText(responder.lastName + ", " + responder.firstName);
+        responderOrganizationField.setText("unknown");
+        responderOrgSuperiorField.setText("unknown");
+        responderLatitudeField.setText(Double.toString(responder.location.latitude));
+        responderLongitudeField.setText(Double.toString(responder.location.longitude));
+        responderHeartbeatField.setText(Float.toString(responder.heartRate));
+        responderOrgSubordinatesField.setText("unknown");
+        responderIncidentIdField.setText(responder.sceneID);
+        responderIncidentSuperiorField.setText("unknown");
+        responderIncidentSubordinatesField.setText("unknown");
+
+        // Return the root view
+        return root;
     }
 
     @Override
