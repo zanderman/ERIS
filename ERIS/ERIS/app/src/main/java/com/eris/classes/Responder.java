0af83154d532e9f66d0c8801f167af9a5b394394
wschrag <wschrag@vt.edu>
2016-10-18 18:56:07 -0400

    Added methods to responder.

--- ERIS/ERIS/app/src/main/java/com/eris/classes/Responder.java
+++ ERIS/ERIS/app/src/main/java/com/eris/classes/Responder.java
@@ -3,6 +3,8 @@ package com.eris.classes;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 
+import java.util.List;
+
 
 public class Responder {
 
@@ -16,7 +18,17 @@ public class Responder {
     public LatLng location;
     public String firstName;
     public String lastName;
+    public String name;
     public Marker marker;
+
+    private String organization;
+    private List<String> heartrateRecord;
+    private String orgSuperior;
+    private List<String> orgSubordinates;
+    private String latitude;
+    private String longitude;
+    private String incidentSuperior;
+    private List<String> incidentSubordinates;
 
     /**
      * Constructs new responder objects.
@@ -63,35 +75,36 @@ public class Responder {
         this.rank = rank;
     }
 
+    public Responder(String userID, String name,  String organization, List<String> heartrateRecord,
+                     String orgSuperior,  List<String> orgSubordinates, String latitude,
+                     String longitude, String sceneID, String incidentSuperior,
+                     List<String> incidentSubordinates) {
+        this.userID = userID;
+        this.name = name;
+        this.organization = organization;
+        this.heartrateRecord = heartrateRecord;
+        this.orgSuperior = orgSuperior;
+        this.orgSubordinates = orgSubordinates;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.sceneID = sceneID;
+        this.incidentSuperior = incidentSuperior;
+        this.incidentSubordinates = incidentSubordinates;
+    }
+
     public String getUserID() {
         return this.userID;
     }
-
     public String getSceneID() {
         return this.sceneID;
     }
+    public String getOrganization() {return  this.organization;}
+    public List<String> getHeartrateRecord() {return this.heartrateRecord;}
+    public String getOrgSuperior() {return  this.orgSuperior;}
+    public List<String> getOrgSubordinates() {return this.getOrgSubordinates();}
+    public String getLatitude() {return this.latitude;}
+    public String getLongitude() {return  this.longitude;}
+    public String getIncidentSuperior() {return  this.incidentSuperior;}
+    public List<String> getIncidentSubordinates() {return this.incidentSubordinates;}
 
-    public float getHeartRate() {
-        return this.heartRate;
-    }
-
-    public String getRank() {
-        return this.rank;
-    }
-
-    public LatLng getLocation() {
-        return this.location;
-    }
-
-    public String getFirstName() {
-        return this.firstName;
-    }
-
-    public String getLastName() {
-        return this.lastName;
-    }
-
-    public Marker getMarker() {
-        return this.marker;
-    }
 }
