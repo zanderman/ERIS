41e50e66e3a2969cfb4374def7b26df1d6f0c5ce
dpickle2 <dpickle29@gmail.com>
2016-10-18 23:07:19 -0400

    Add sample database scan/filter for demo

---
+++ ERIS/ERIS/app/src/main/java/com/eris/adapters/ResponderDatabaseListAdapter.java
@@ -0,0 +1,53 @@
+package com.eris.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.eris.R;
+import com.eris.classes.Responder;
+
+public class ResponderDatabaseListAdapter extends ArrayAdapter<Responder> {
+
+    private LayoutInflater inflater;
+
+    public ResponderDatabaseListAdapter(Context context) {
+        super(context, R.layout.fragment_demo_responder_database);
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+//        return super.getView(position, convertView, parent);
+
+        // Get responder object.
+        Responder responder = getItem(position);
+
+        // Gain access to view.
+        View root = inflater.inflate(R.layout.list_item_responder, parent, false);
+
+        // Get view elements.
+        TextView nameTextView = (TextView) root.findViewById(R.id.nameTextView);
+        TextView uidTextView = (TextView) root.findViewById(R.id.uidTextView);
+        TextView healthTextView = (TextView) root.findViewById(R.id.healthTextView);
+        TextView locationTextView = (TextView) root.findViewById(R.id.locationTextView);
+
+        // Set content of view elements.
+        nameTextView.setText(responder.firstName + " " + responder.lastName);
+        uidTextView.setText(responder.userID);
+        healthTextView.setText(responder.heartRate + " bpm");
+        if (responder.location != null) {
+            locationTextView.setText(responder.location.latitude + ", " + responder.location.longitude);
+        }
+        else {
+            locationTextView.setText("location unknown");
+        }
+
+        // Return the altered view.
+        return root;
+
+    }
+}
--- ERIS/ERIS/app/src/main/java/com/eris/fragments/DemoResponderDatabaseFragment.java
+++ ERIS/ERIS/app/src/main/java/com/eris/fragments/DemoResponderDatabaseFragment.java
@@ -3,6 +3,7 @@ package com.eris.fragments;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -17,10 +18,18 @@ import android.widget.Toast;
 
 import com.amazonaws.mobile.AWSMobileClient;
 import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBMapper;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBQueryExpression;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBScanExpression;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.PaginatedQueryList;
 import com.amazonaws.models.nosql.UserDataDO;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.eris.R;
-import com.eris.adapters.ResponderListAdapter;
+import com.eris.adapters.ResponderDatabaseListAdapter;
 import com.eris.classes.Responder;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 /**
@@ -30,11 +39,10 @@ import com.eris.classes.Responder;
 public class DemoResponderDatabaseFragment extends Fragment{
     private final String TAG = DemoResponderDatabaseFragment.class.getSimpleName();
 
-    private final DynamoDBMapper mapper = AWSMobileClient.defaultMobileClient().getDynamoDBMapper();
+    private final DynamoDBMapper mapper;
 
-
-    private ResponderListAdapter responderListAdapter;
-    private ListView responderListView;
+    private ResponderDatabaseListAdapter responderDatabaseListAdapter;
+    private ListView responderDatabaseListView;
     private TextView userIdKeyTextView;
     private EditText responderNameField;
     private EditText responderOrganizationField;
@@ -48,16 +56,12 @@ public class DemoResponderDatabaseFragment extends Fragment{
     private EditText responderIncidentSubordinatesField;
 
     public DemoResponderDatabaseFragment() {
-        //Required?
+        mapper = AWSMobileClient.defaultMobileClient().getDynamoDBMapper();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //result.setOrganization("EMS");
-        //result = mapper.load(UserDataDO.class, "4093820716");
-        FetchTask task = new FetchTask();
-        task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
 
     @Override
@@ -66,19 +70,8 @@ public class DemoResponderDatabaseFragment extends Fragment{
 
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_demo_responder_database, container, false);
+
         // Initialize UI elements
-        responderListView = (ListView) root.findViewById(R.id.responderListView);
-        responderListView.setAdapter(responderListAdapter);
-        responderListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                // Show responder location on map.
-                final Responder responder = responderListAdapter.getItem(i);
-                //Toast.makeText(getActivity(),"Clicked: " + responder.firstName + " " + responder.lastName,Toast.LENGTH_SHORT).show();
-            }
-        });
-
-
         userIdKeyTextView = (TextView) root.findViewById(R.id.responderIdField);
         responderNameField = (EditText) root.findViewById(R.id.responderNameField);
         responderOrganizationField = (EditText) root.findViewById(R.id.responderOrganizationField);
@@ -90,6 +83,33 @@ public class DemoResponderDatabaseFragment extends Fragment{
         responderIncidentIdField = (EditText) root.findViewById(R.id.responderIncidentIdField);
         responderIncidentSuperiorField = (EditText) root.findViewById(R.id.responderIncidentSuperiorField);
         responderIncidentSubordinatesField = (EditText) root.findViewById(R.id.responderIncidentSubordinatesField);
+
+
+        responderDatabaseListAdapter = new ResponderDatabaseListAdapter(getActivity());
+        responderDatabaseListView = (ListView) root.findViewById(R.id.responderDatabaseListView);
+        responderDatabaseListView.setAdapter(responderDatabaseListAdapter);
+        responderDatabaseListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                // Update the scroll view's content to match the selected responder
+                final Responder responder = responderDatabaseListAdapter.getItem(i);
+
+                userIdKeyTextView.setText(responder.getUserID());
+                responderNameField.setText(responder.getName());
+                responderOrganizationField.setText(responder.getOrganization());
+                responderOrgSuperiorField.setText(responder.getOrgSuperior());
+                responderLatitudeField.setText(responder.getLatitude());
+                responderLongitudeField.setText(responder.getLongitude());
+                responderHeartbeatField.setText(responder.getHeartrateRecord().get(0));
+                responderOrgSubordinatesField.setText(responder.getOrgSubordinates().toString());
+                responderIncidentIdField.setText(responder.getSceneID());
+                responderIncidentSuperiorField.setText(responder.getIncidentSuperior());
+                responderIncidentSubordinatesField.setText(responder.getIncidentSubordinates().toString());
+            }
+        });
+
+        FetchTask task = new FetchTask();
+        task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
 
         // Return the root view
         return root;
@@ -108,29 +128,37 @@ public class DemoResponderDatabaseFragment extends Fragment{
 
     private class FetchTask extends AsyncTask<Void, Void, Void> {
 
+        ArrayList<Responder> data = new ArrayList<Responder>();
+
         @Override
         protected Void doInBackground(Void... voids) {
-            final UserDataDO item = mapper.load(UserDataDO.class, "4093820716");
-            Log.d(TAG, item.getUserId());
+            // Perform a scan using a filter condition.
+            HashMap<String, AttributeValue> attrValues = new HashMap<>();
+            attrValues.put(":org", new AttributeValue().withS("EMS"));
 
-            getActivity().runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    userIdKeyTextView.setText(item.getUserId());
-                    responderNameField.setText(item.getName());
-                    responderOrganizationField.setText(item.getOrganization());
-                    responderOrgSuperiorField.setText(item.getOrgSuperior());
-                    responderLatitudeField.setText(item.getLatitude());
-                    responderLongitudeField.setText(item.getLongitude());
-                    responderHeartbeatField.setText(item.getHeartbeatRecord().toString());
-                    responderOrgSubordinatesField.setText(item.getOrgSubordinates().toString());
-                    responderIncidentIdField.setText(item.getCurrentIncidentId());
-                    responderIncidentSuperiorField.setText(item.getIncidentSuperior());
-                    responderIncidentSubordinatesField.setText(item.getIncidentSubordinates().toString());
-                }
-            });
+            DynamoDBScanExpression scanExpr = new DynamoDBScanExpression()
+                    .withFilterExpression("organization = :org")
+                    .withExpressionAttributeValues(attrValues);
+            List<UserDataDO> results = mapper.scan(UserDataDO.class, scanExpr);
+
+            for (UserDataDO user : results ) {
+                data.add(new Responder(user.getUserId(), user.getName(), user.getOrganization(),
+                        user.getHeartbeatRecord(), user.getOrgSuperior(), user.getOrgSubordinates(),
+                        user.getLatitude(), user.getLongitude(), user.getCurrentIncidentId(),
+                        user.getIncidentSuperior(), user.getIncidentSubordinates()));
+            }
+
             return null;
         }
 
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            // Add each data element to the list adapter.
+            for ( Responder element : data ) {
+                responderDatabaseListAdapter.add(element);
+            }
+        }
     }
 }
