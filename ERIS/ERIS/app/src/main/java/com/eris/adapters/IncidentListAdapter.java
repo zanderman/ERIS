f55661699d71fe1a08e368bdf7c383e4f2acd047
Alexander DeRieux <acd1797@vt.edu>
2016-10-22 13:59:39 -0400

    Created adapter for list of incidents.

---
+++ ERIS/ERIS/app/src/main/java/com/eris/adapters/IncidentListAdapter.java
@@ -0,0 +1,43 @@
+package com.eris.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import com.eris.R;
+import com.eris.classes.Incident;
+
+
+/**
+ * IncidentListAdapter
+ *
+ * Description
+ *  Adapter for controlling a list of incidents.
+ */
+public class IncidentListAdapter extends ArrayAdapter<Incident> {
+
+    /*
+     * Private Members
+     */
+    private LayoutInflater inflater;
+
+    /**
+     * Constructor
+     *
+     * Description:
+     *  Creates the layout for the adapter.
+     *
+     * @param context Context in which the adapter will be used.
+     */
+    public IncidentListAdapter(Context context) {
+        super(context, R.layout.list_item_incident);
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return super.getView(position, convertView, parent);
+    }
+}
