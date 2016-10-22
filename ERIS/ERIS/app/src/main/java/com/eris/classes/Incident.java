94309351dbf3e49825d5c32480b2ab138f24b93a
Alexander DeRieux <acd1797@vt.edu>
2016-10-22 13:32:07 -0400

    Created basic constructor and public members. The incident is identified by the following info: - String : id - String : description - String : address - String : location

--- ERIS/ERIS/app/src/main/java/com/eris/classes/Incident.java
+++ ERIS/ERIS/app/src/main/java/com/eris/classes/Incident.java
@@ -1,5 +1,7 @@
 package com.eris.classes;
 
+import com.google.android.gms.maps.model.LatLng;
+
 /**
  * Incident
  *
@@ -7,4 +9,31 @@ package com.eris.classes;
  *  Represents an incident. Contains all pertinent information regarding the incident.
  */
 public class Incident {
+
+    /*
+     * Public Members
+     */
+    public String id;
+    public String description;
+    public String address;
+    public LatLng location;
+
+
+    /**
+     * Constructor
+     *
+     * Description:
+     *  Only initializes the description of the incident.
+     *
+     * @param description Information about the incident.
+     * @param address Address of the incident.
+     * @param location Latitude/Longitude of the incident.
+     */
+    public Incident(String id, String description, String address, LatLng location) {
+        this.id = id;
+        this.description = description;
+        this.address = address;
+        this.location = location;
+    }
+
 }
