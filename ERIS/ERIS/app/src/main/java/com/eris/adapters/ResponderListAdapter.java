9e98225cd80ad45580c64cde0b10dacbcb4a4f75
dpickle2 <dpickle29@gmail.com>
2016-10-30 16:35:20 -0400

    Update ResponderListAdapter.java
    Class has been refactored for use in the responder view
    section of the Incident map view.

--- ERIS/ERIS/app/src/main/java/com/eris/adapters/ResponderListAdapter.java
+++ ERIS/ERIS/app/src/main/java/com/eris/adapters/ResponderListAdapter.java
@@ -1,11 +1,13 @@
 package com.eris.adapters;
 
 import android.content.Context;
+import android.graphics.drawable.Icon;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.ImageView;
 
 import com.eris.R;
 import com.eris.classes.Responder;
@@ -21,7 +23,6 @@ public class ResponderListAdapter extends ArrayAdapter<Responder> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-//        return super.getView(position, convertView, parent);
 
         // Get responder object.
         Responder responder = getItem(position);
@@ -30,21 +31,14 @@ public class ResponderListAdapter extends ArrayAdapter<Responder> {
         View root = inflater.inflate(R.layout.list_item_responder, parent, false);
 
         // Get view elements.
-        TextView nameTextView = (TextView) root.findViewById(R.id.nameTextView);
-        TextView uidTextView = (TextView) root.findViewById(R.id.uidTextView);
-        TextView healthTextView = (TextView) root.findViewById(R.id.healthTextView);
-        TextView locationTextView = (TextView) root.findViewById(R.id.locationTextView);
+        TextView nameTextView = (TextView) root.findViewById(R.id.responder_name);
+        TextView heartRateTextView = (TextView) root.findViewById(R.id.responder_heart_rate);
+        ImageView userPhotoImageView = (ImageView) root.findViewById(R.id.responder_image);
 
         // Set content of view elements.
         nameTextView.setText(responder.firstName + " " + responder.lastName);
-        uidTextView.setText(responder.userID);
-        healthTextView.setText(responder.heartRate + " bpm");
-        if (responder.location != null) {
-            locationTextView.setText(responder.location.latitude + ", " + responder.location.longitude);
-        }
-        else {
-            locationTextView.setText("location unknown");
-        }
+        heartRateTextView.setText("Heart rate: " + Float.toString(responder.heartRate));
+        userPhotoImageView.setImageResource(R.mipmap.ic_donut_large_black_24dp);
 
         // Return the altered view.
         return root;
